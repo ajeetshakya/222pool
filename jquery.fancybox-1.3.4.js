3217a6b8e5f99799bb276f8e41f9d9ef923f7c45
ajeet <azeet_shakya@hotmail.com>
2013-12-17 15:25:26 +0545

    asd fvv

--- jquery.fancybox-1.3.4.js
+++ jquery.fancybox-1.3.4.js
@@ -12,7 +12,7 @@
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html asdfsd
+ *   http://www.gnu.org/licenses/gpl.html asdfsd asdf asdf 
  */
 
 ;(function($) {
